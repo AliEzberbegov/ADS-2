1024445654a844fad84a49674ced2a80778f7d4a
Ali <76096715+AliEzberbegov@users.noreply.github.com>
2021-04-07 16:15:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,68 +1,68 @@
 // Copyright 2021 NNTU-CS
-int countPairs1(int *arr, int len, int value){
+int countPairs1(int *arr, int len, int value) {
   int count = 0;
-  for (int i = 0; i < len - 1; i++){
-    for (int j = i + 1; j < len; j++){
-      if (arr[i] + arr[j] == value){
+  for (int i = 0; i < len - 1; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
         count++;
       }
     }
   }
   return count;
 }
-int countPairs2(int *arr, int len, int value){
+int countPairs2(int *arr, int len, int value) {
   int count = 0;
   int left = 0;
   int right = len - 1;
   int center;
-  while (left < (right - 1)){
+  while (left < right - 1) {
     center = (right + left) / 2;
-    if (arr[center] > value){
+    if (arr[center] > value) {
       right = center;
-    } else if (arr[center] <= value){
+    } else if (arr[center] <= value) {
       left = center;
     }
   }
   len = right - 1;
-  for (i = len; i >= 0; i--){
-    for (j = 0; j < i; j++){
-      if (arr[i] + arr[j] == value){
+  for (i = len; i >= 0; i--) {
+    for (j = 0; j < i; j++) {
+      if (arr[i] + arr[j] == value) {
         count++;
       }
-      if (arr[i] + arr[j] > value){
+      if (arr[i] + arr[j] > value) {
         break;
       }
     }
   }
   return count; 
 }
-int countPairs3(int *arr, int len, int value){
+int countPairs3(int *arr, int len, int value) {
   int generalCount = 0;
   int left = 0;
   int right = len - 1;
   int center;
-  while (left < (right - 1)){
+  while (left < right - 1) {
     center = (right + left) / 2;
-    if (arr[center] > value){
+    if (arr[center] > value) {
       right = center;
-    } else if (arr[center] <= value){
+    } else if (arr[center] <= value) {
       left = center;
     }
   }
   len = right - 1;
-  for (int i = 0; i < len; i++){
+  for (int i = 0; i < len; i++) {
     left = i + 1;
     right = len - 1;
     int innerCount = 0;
-    while (left < right){
+    while (left < right) {
       center = (left + right) / 2;
-      if (arr[center] >= (value - arr[i])){
+      if (arr[center] >= value - arr[i]) {
       right = center;
-      } else if (arr[center] < (value - arr[i])){
+      } else if (arr[center] < value - arr[i]) {
       left = center + 1;
       }
     }
-    while (arr[left] == (value - arr[i])){
+    while (arr[left] == value - arr[i]) {
     innerCount++;
     left++;
     }
